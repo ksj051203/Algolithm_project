7cb4ce85fc7ce6f36a06085809c11ab18fd27127
ksj051203 <ksj051203@naver.com>
2021-12-08 15:05:43 +0900

    구조체 선언

--- Parking_System.cpp
+++ Parking_System.cpp
@@ -5,6 +5,19 @@
 #define FLOOR 2
 #define ROOM 10
 
+struct carInfo{
+	char carNum[5];
+	int number;
+	int floor;
+	char carOwner[4];
+	
+};
+
+struct floorInfo{
+	struct carInfo rooms[ROOM];
+};
+
+
 void printMain(void)
 {
     printf("1. 주차\n");
@@ -15,6 +28,12 @@ void printMain(void)
     printf("> 번호선택 : ");
 }
 
+void menu1(struct floorInfo *input, struct carInfo * car){
+	scanf("이름 : %s", carOwner);
+	scanf("차 번호 : %s", carNum);
+	scanf("층 선택 : %d", &floor);
+	scanf("번호 선택 : %d", &number);
+}
 int main(){
     int rsvNum = 0;
     int answer;
