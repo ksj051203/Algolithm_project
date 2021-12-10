03552d9775ecd22e184f82d6059351e73dff64b4
ksj051203 <ksj051203@naver.com>
2021-12-10 10:58:36 +0900

    menu1 함수 수정 및 printALL 함수 생성

--- Parking_System.cpp
+++ Parking_System.cpp
@@ -1,20 +1,15 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #define FLOOR 2
 #define ROOM 10
 struct carInfo{
-	char carNum[5];
-	int number;
-	int floor;
-	char carOwner[4];
-	
+    char carNum[5];
+    char carOwner[4];
 };
-
 struct floorInfo{
-	struct carInfo rooms[ROOM];
+    struct carInfo *rooms[ROOM];
 };
-
-
 void printMain(void)
 {
     printf("1. 주차\n");
@@ -24,53 +19,65 @@ void printMain(void)
     printf("0. 종료\n");
     printf("> 번호선택 : ");
 }
-//struct floorInfo input, 
-void menu1(struct carInfo *car){
-	printf("본인의 이름을 입력해주세요 : ");
-	scanf("%s", car->carOwner);
-	printf("차 번호를 입력해주세요 : "); 
-	scanf("%s", car->carNum);
-	printf("주차할 층을 선택해주세요 : "); 
-	scanf("%d", &car->floor);
-	printf("주차 할 번호를 선택해주세요 : ");
-	scanf("%d", &car->number);
-	printf("주차를 완료하였습니다!");
+void menu1(struct floorInfo * floor, struct carInfo *car){
+    /*car = (carInfo *)malloc(sizeof(carInfo));
+   floor = (floorInfo *)malloc(sizeof(floorInfo));*/
+    int n;
+    printf("주차 할 번호를 선택해주세요: ");
+    scanf("%d", &n);
+    printf("본인의 이름을 입력해주세요 : ");
+    scanf("%s",floor->rooms[n]->carOwner);
+    printf("차 번호를 입력해주세요 : "); 
+    scanf("%s", floor->rooms[n]->carNum);
+    printf("주차를 완료하였습니다!\n");
 }
+
+void printAll(struct carInfo * car){
+   printf("[B1층]====================================================================\n");
+   for(int i = 0;i < 10;i++){
+      if(i == 5)printf("\n");
+      printf("[   %d  ] ",(car+i)->carNum);   
+   }
+   printf("\n");
+}
+void menu2(struct floorInfo * floor){
+
+}
+
+
 int main(){
-	struct carInfo car[10];
-	struct floorInfo floor[2];
+    struct floorInfo* floor;
+    struct carInfo* car;
+    
+    car = (carInfo *)malloc(sizeof(carInfo));
+   floor = (floorInfo *)malloc(sizeof(floorInfo));
     int answer;
-  	while (1)
-    {
+    while (1){
         printMain();
-        scanf("%c", &answer);
-        fflush(stdin);
+        scanf("%d", &answer);
  
-        if (answer == '1')
+        if (answer == 1)
         {
-            menu1(car);
+            menu1(floor, car);
         }
-        else if (answer == '2')
+        else if (answer == 2)
         {
-            printf("이태현 바");
+           
         }
-        else if (answer == '3')
+        else if (answer == 3)
         {
-            printf("이태현 바ㅂ");
+           
         }
-        else if (answer == '4')
+        else if (answer == 4)
         {
-            printf("이태현 바22");
+            printAll(car);
         }
-        else if (answer == '0')
+        else if (answer == 0)
             break;
- 
         else
         {
-            printf("잘못 입력하셨습니다. 다시 입력해주세요.");
+            printf("잘못 입력하셨습니다. 다시 입력해주세요.\n");
+            
         }
     }
 }
-
- 
-
