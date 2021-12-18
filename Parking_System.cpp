e030bd7f67283fdcaa03c3b0c68ef68f74bc3851
ksj051203 <ksj051203@naver.com>
2021-12-18 23:15:46 +0900

    스택 주차장 전체 구현

--- Parking_System.cpp
+++ Parking_System.cpp
@@ -4,7 +4,9 @@
 #include <windows.h>
 #define FLOOR 2
 #define ROOM 10
+#define SIZE 10
 int count[11];
+int top = -1;
 typedef struct carInfo{
    char carNum[100]={};
    char carOwner[100]={};
@@ -26,7 +28,16 @@ void printMain(void)
     printf("==================================================\n");
     printf("> 번호선택 : ");
 }
-
+void printMain2(void)
+{
+    printf("==================================================\n");
+    printf("1. 주차\n");
+    printf("2. 출차\n");
+    printf("3. 주차현황 출력\n"); 
+    printf("0. 종료\n");
+    printf("==================================================\n");
+    printf("> 번호선택 : ");
+}
 void menu1(fl * floor){
     int n;
     printf("주차 할 번호를 선택해주세요 (1~10까지 입력): ");
@@ -38,12 +49,12 @@ void menu1(fl * floor){
         }
         count[n-1]++;
         if(n>=1&&n<11){
-	        printf("본인의 이름을 입력해주세요 : ");
-	        scanf("%s",floor->rooms[n-1].carOwner);
-	        printf("차 번호를 입력해주세요: "); 
-	        scanf("%s", floor->rooms[n-1].carNum);
-	        printf("주차를 완료하였습니다!\n");
-            break;
+           printf("본인의 이름을 입력해주세요 : ");
+           scanf("%s",floor->rooms[n-1].carOwner);
+           printf("차 번호를 입력해주세요: "); 
+           scanf("%s", floor->rooms[n-1].carNum);
+           printf("주차를 완료하였습니다!\n");
+              break;
         }else{
             printf("번호를 다시 입력해주세요^^ : ");
             continue; 
@@ -86,38 +97,133 @@ void menu3(fl * floor){
 void printAll(fl * floor){
     printf("[B1층]============================================\n");
     for(int i = 0; i < 10;i++){
-    	if(i == 5)printf("\n"){
-      		printf("[   %s  ] ",floor->rooms[i].carNum);
-   		}
+    	if(i == 5)printf("\n");
+      	printf("[   %s  ] ",floor->rooms[i].carNum);
+   	}
    	printf("\n");
+}
+
+void printAll2(fl * floor2){
+    printf("[지상]============================================\n");
+    for(int i = 0; i < 10;i++){
+    	printf("[   %s  ] ",floor2->rooms[i].carNum);
+   	}
+   	printf("\n");
+}
+
+int isFull() {    // 스택이 포화 상태인지 검사하는 함수
+	if(top >= SIZE - 1){   // top이 최대 크기와 같거나 클 경우
+    	return 1;  // 1 반환 (true)
+	}
+	return 0;   // 그렇지 않을 경우 0 반환 (false)
+}
+
+void put_car(fl * floor2){
+    int n1;
+	if(!isFull()){
+    	top++;
+    	printf("본인의 이름을 입력해주세요 : ");
+    	scanf("%s", floor2->rooms[top].carOwner);
+		printf("차 번호를 입력해주세요: "); 
+    	scanf("%s", floor2->rooms[top].carNum);
+    	printf("주차를 완료하였습니다!\n");
+   	}
+   	else {
+    	printf("주차공간이 가득 찼습니다.");
+   	}
+ }
+
+int isEmpty() {    
+	if(top == -1) {   
+    	return 1;  
+   	}
+   	return 0;   
+}
+
+void pop(fl * floor2) {
+	char test[100]={};
+    if(!isEmpty()){
+        printf("%s님의 %s번 출차를 완료하였습니다!\n", floor2->rooms[top].carOwner, floor2->rooms[top].carNum);
+        strcpy(floor2->rooms[top].carNum, test);
+        top--;
+   }
+   else printf("주차장이 비어있습니다.\n");
 }
 
 int main(){
    	system("주차 관리 프로그램");
    	system("color 8f");
 	fl floor;
-	int answer;
-	    while(1){
-	        printMain();
-	        scanf("%d", &answer);
-	        switch(answer){
-	            case 1:
-	                menu1(&floor);
-	                break;
-	            case 2:
-	                menu2(&floor);
-	                break;
-	            case 3:
-	                menu3(&floor); 
-	                break;
-	            case 4:
-	                printAll(&floor);
-	                break;
-	            case 0:
-	                printf("안녕히가세요!\n");
-	                break;
-	            default :
-	                printf("잘못 입력하셨습니다. 다시 입력해주세요.\n");
-            }
+   	fl floor2;
+	int answer, answer2, select;
+    while(1){
+	    printf("==================================================\n");
+	    printf("0. 종료\n");
+	    printf("1. 일반 주차장\n");
+	    printf("2. 스택 주차장\n"); 
+	    printf("==================================================\n");
+	    printf("> 번호선택 : ");
+	    scanf("%d", &select);
+	    if(select==1){
+	    	while(1){
+	            printMain();
+	            scanf("%d", &answer);
+	            switch(answer){
+	                case 1:
+	                    menu1(&floor);
+	                    break;
+	                case 2:
+	                    menu2(&floor);
+	                    break;
+	                case 3:
+	                    menu3(&floor); 
+	                    break;
+	                case 4:
+	                    printAll(&floor);
+	                    break;
+	                case 0:
+	                    printf("안녕히가세요!\n");
+	                    break;
+	                default :
+	                    printf("잘못 입력하셨습니다. 다시 입력해주세요.\n");
+            	}
+            	if(answer == 0){
+					break;
+        		}
+        	}
+    	}
+        
+       	else if(select==2){
+          	while(1){
+            	printMain2();
+            	scanf("%d", &answer2);
+            	switch(answer2){
+                case 1:
+                   	put_car(&floor2);
+                   	break;
+                case 2:
+                   	pop(&floor2); 
+                case 3:
+                   	printAll2(&floor2);
+                    break;
+                case 0:
+                   	printf("안녕히가세요!");
+                    break;
+                default :
+                   	printf("잘못 입력하셨습니다. 다시 입력해주세요.\n");
+            	}
+            	if(answer2 == 0){
+					break;
+        		}
+         	}
         }
-    }
+       else if(select==0){
+           printf("안녕히가세요!\n");
+           break;
+       }
+       else{
+           printf("다시 입력해주세요.\n");
+           continue;
+      }
+   }
+}
